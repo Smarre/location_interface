16a7ebd632e0a13e93a40e0a3226d138c42836d3
Samu Voutilainen <smar@smar.fi>
2014-01-08 11:58:32 +0200

    exception testing

--- lib/location_interface.rb
+++ lib/location_interface.rb
@@ -3,13 +3,20 @@ require "sinatra"
 require "sinatra/json"
 require "nominatim"
 require "psych"
+require "net/smtp"
 
 class LocationInterface < Sinatra::Base
+    
 
     helpers Sinatra::JSON
 
     before do
         parse_config
+    end
+
+    error Exception do
+        puts "argh"
+        exit 1
     end
 
     get "/" do
@@ -27,6 +34,7 @@ class LocationInterface < Sinatra::Base
     end
 
     post "/reverse" do
+        foobar
         place = Nominatim.reverse(params["latitude"], params["longitude"]).address_details(true).fetch
         return json "Nothing found for given coordinates" if place.nil?
         address = place.address
@@ -53,4 +61,31 @@ class LocationInterface < Sinatra::Base
     end
 
     run! if app_file == $0
+
+rescue Exception => e
+    puts "nya"
+    exit 1
+    contents = Psych.load_file "config/config.yaml"
+
+    unless contents["error_email"].nil?
+        Net::SMTP.start contents["error_email"]["server"], contents["error_email"]["port"],
+                contents["error_email"]["username"], contents["error_email"]["password"], contents["error_email"]["method"] do |smtp|
+            sender_email = "dummy@email.nya"
+            message = <<-EOF
+            From: Location interface error catcher <#{sender_email}>
+            To: <#{contents["error_email"]["email"]}>
+            Subject: Error in location interface
+
+            There was an error in location interface:
+
+            #{e.message}
+
+            Backtrace:
+            #{e.backtrace}
+            EOF
+            smtp.send_message message, sender_email, contents["error_email"]["email"]
+        end
+    end
+
+    raise e
 end
