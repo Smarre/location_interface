9bf5cf2f6e2cbc93c299e13c3a280256a9a42226
Samu Voutilainen <smar@smar.fi>
2015-02-09 14:09:16 +0200

    address.rb: Handle nil input properly

--- lib/address.rb
+++ lib/address.rb
@@ -10,33 +10,39 @@ class Address
     @@sqlite = nil
 
     # Splits street address and returns hash of street address, street number and house number
+    #
+    # Returns nil in case of street.nil?
     def self.split_street street
+
         street_name = nil
         street_number = nil
         house_number = nil
 
-        street.gsub! /,/, ""
-        splitted = street.split " "
-        iter = splitted.dup.each
-        street_name = iter.next
-        splitted.delete_at 0
-
-        begin
-        loop do
-            part = iter.next
-            if part[0] =~ /[[:digit:]]/
-                street_number = part
-                splitted.delete_at 0
-                break
-            end
-            street_name += " #{part}"
+        catch(:end) do
+            throw :end if street.nil?
+            street.gsub! /,/, ""
+            splitted = street.split " "
+            iter = splitted.dup.each
+            street_name = iter.next
             splitted.delete_at 0
-        end
-        rescue StopIteration => e
-            # all fine
-        end
 
-        house_number = splitted.join " "
+            begin
+            loop do
+                part = iter.next
+                if part[0] =~ /[[:digit:]]/
+                    street_number = part
+                    splitted.delete_at 0
+                    break
+                end
+                street_name += " #{part}"
+                splitted.delete_at 0
+            end
+            rescue StopIteration => e
+                # all fine
+            end
+
+            house_number = splitted.join " "
+        end
 
         {
             street_name: street_name,
