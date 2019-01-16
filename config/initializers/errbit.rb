37e96d5b468ff6f1fdbb830e577d2722b1c7018d
Samu Voutilainen <smar@smar.fi>
2019-01-16 14:50:31 +0200

    Remove SSL patch

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,20 +1,6 @@
 require "airbrake"
 
-module Patches
-  module Airbrake
-    module SyncSender
-      def build_https(uri)
-        super.tap do |req|
-          req.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        end
-      end
-    end
-  end
-end
-
 settings = Psych.load_file "#{__dir__}/../config.yaml"
-
-Airbrake::SyncSender.prepend(::Patches::Airbrake::SyncSender)
 
 Airbrake.configure do |config|
   config.host = settings["airbrake"]["host"]
